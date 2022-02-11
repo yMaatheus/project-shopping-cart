0d1f2035dee309a526395e1d98c6cc019d953446
Matheus <stvgamer74@gmail.com>
2022-02-11 17:30:15 -0300

    Teste

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,4 +1,5 @@
 const saveCartItems = () => {
+  // console.log(ol.innerHTML);
   const cartItemsList = Array
     .from(document.querySelector('.cart__items').children).map((element) => element.id);
   localStorage.setItem('cartItems', JSON.stringify(cartItemsList));
